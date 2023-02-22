b421b79393bfa780e4320d80236ccbe3bec935cd
Zander Ambrose <alexander.d.ambrose@gmail.com>
2023-02-22 12:47:32 -0500

    Update skills icons.

--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -1,15 +1,14 @@
 import React from "react";
-import { AiFillHtml5 } from "react-icons/ai";
 import { SiTypescript } from "react-icons/si";
 import { SiExpress } from "react-icons/si";
-import { DiCss3 } from "react-icons/di";
 import { DiPython } from "react-icons/di";
 import { SiJavascript } from "react-icons/si";
 import { SiDjango } from "react-icons/si";
-import { DiReact } from "react-icons/di";
+import { TbBrandNextjs } from "react-icons/tb";
 import { DiGit } from "react-icons/di";
-import { SiFirebase } from "react-icons/si";
 import { FaNodeJs } from "react-icons/fa";
+import { FaDocker } from "react-icons/fa";
+import { SiNginx } from "react-icons/si";
 
 const Skills = () => {
   return (
@@ -32,36 +31,28 @@ const Skills = () => {
           <SiDjango size="8rem" color="#0C3C26" />
         </div>
         <div className="skillsGridItem">
+          <h1>React/NextJs</h1>
+          <TbBrandNextjs size="8rem" color="#000000" />
+        </div>
+        <div className="skillsGridItem">
           <h1>Express</h1>
           <SiExpress size="8rem" color="#000000" />
         </div>
         <div className="skillsGridItem">
-          <h1>HTML</h1>
-          <AiFillHtml5 size="8rem" color="#FFA500" />
-        </div>
-        <div className="skillsGridItem">
-          <h1>CSS</h1>
-          <DiCss3 size="8rem" color="#2B60DE" />
-        </div>
-        <div className="skillsGridItem">
-          <h1>Javascript</h1>
-          <SiJavascript size="8rem" color="#F0DB4F" />
-        </div>
-        <div className="skillsGridItem">
-          <h1>React</h1>
-          <DiReact size="8rem" color="#61DBFB" />
+          <h1>Node JS</h1>
+          <FaNodeJs size="8rem" color="#3c873a" />
         </div>
         <div className="skillsGridItem">
           <h1>Git</h1>
           <DiGit size="8rem" color="#F1502F" />
         </div>
         <div className="skillsGridItem">
-          <h1>Firebase</h1>
-          <SiFirebase size="8rem" color="#FFA500" />
+          <h1>Docker</h1>
+          <FaDocker size="8rem" color="#0073ec" />
         </div>
         <div className="skillsGridItem">
-          <h1>Node JS</h1>
-          <FaNodeJs size="8rem" color="#3c873a" />
+          <h1>Nginx</h1>
+          <SiNginx size="8rem" color="#009639" />
         </div>
       </div>
     </section>
